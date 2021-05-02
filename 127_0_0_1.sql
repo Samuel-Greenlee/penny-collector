e76c99525f0d2ef3b74ed72fcda389b7bad8fcf5
Samuel Greenlee <sg630912@southeast.edu>
2021-05-01 22:28:04 -0500

    This will be the final push for the database for program 3. If I do not make any further pushes from here on out, then this will be marked as my final push. I am glad that I got a chance to build such an amazing project.

--- 127_0_0_1.sql
+++ 127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 29, 2021 at 12:38 AM
+-- Generation Time: May 02, 2021 at 05:24 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.13
 
@@ -43,8 +43,26 @@ CREATE TABLE `penny` (
 --
 
 INSERT INTO `penny` (`pennyID`, `userName`, `pennyMint`, `pennyCondition`, `pennyAmount`, `pennyYear`) VALUES
-(7, 'Sam', 'Seattle', 'Good', '55', 1945),
-(8, 'Sam', 'Denver', 'Great', '80', 1980);
+(9, 'Sam', 'San Francisco', 'Good', '5', 1934),
+(10, 'Sam', 'Denver', 'Great', '8', 1945),
+(11, 'Sam', 'Philadelphia', 'Bad', '6', 2009),
+(12, 'Sam', 'Denver', 'Great', '10', 1944),
+(13, 'Sam', 'San Francisco', 'Good', '7', 1980),
+(14, 'Gram', 'San Francisco', 'Good', '7', 1980),
+(15, 'Gram', 'Denver', 'Great', '5', 1944),
+(16, 'Gram', 'Philadelphia', 'Good', '4', 1945),
+(17, 'Gram', 'Denver', 'Bad', '9', 1930),
+(18, 'Gram', 'Denver', 'Great', '10', 1937),
+(19, 'Darren', 'Philadelphia', 'Great', '7', 1991),
+(20, 'Darren', 'Denver', 'Bad', '8', 1999),
+(21, 'Darren', 'Denver', 'Great', '7', 1944),
+(22, 'Darren', 'Philadelphia', 'Good', '8', 1937),
+(25, 'Darren', 'Denver', 'Great', '5', 1980),
+(26, 'Billy', 'San Francisco', 'Great', '5', 1945),
+(27, 'Billy', 'Denver', 'Good', '8', 1976),
+(28, 'Billy', 'Philadelphia', 'Bad', '7', 1930),
+(29, 'Billy', 'Denver', 'Great', '7', 1991),
+(30, 'Billy', 'San Francisco', 'Bad', '9', 1966);
 
 -- --------------------------------------------------------
 
@@ -54,7 +72,7 @@ INSERT INTO `penny` (`pennyID`, `userName`, `pennyMint`, `pennyCondition`, `penn
 
 CREATE TABLE `user` (
   `userName` varchar(30) CHARACTER SET utf8 NOT NULL,
-  `password` varchar(30) CHARACTER SET utf8 NOT NULL
+  `password` varchar(255) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -62,15 +80,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`userName`, `password`) VALUES
-('Bob', 'Bobbbbbb1'),
-('Bobby', 'BobBob88*'),
-('Fred', 'Bill888!'),
-('Jill', 'Jelly&88!'),
-('Sally', 'Sally888!!!'),
-('Sam', 'Sammy888!!!'),
-('Sim', 'Sim88888*'),
-('Ted', 'Teddy999!!!'),
-('Test', 'T8t88888');
+('Billy', '$2y$10$U5cxfoPviLO2X7DtfhOyLOqQd0TIv12rXNJo5TS.YOnreUYpXbMw2'),
+('Darren', '$2y$10$ouUAISIlHmRJMRSXW13tEOdTfmZXGrbOHkuEGMhDrRLFHXbq2ACRu'),
+('Gram', '$2y$10$xYuvzV/R4Tm1KPqkQjvy2eGNRLS6gr1BXTPRLOfgL2.rZpqNk7Z3S'),
+('Sam', '$2y$10$U5nH7EmJWVYbRTO91udO1eOtvi0XC1cXUdwmvkD0vCIJqX0NPFmoe');
 
 --
 -- Indexes for dumped tables
@@ -97,7 +110,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `penny`
 --
 ALTER TABLE `penny`
-  MODIFY `pennyID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `pennyID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- Constraints for dumped tables
